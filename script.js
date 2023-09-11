01ac836a9c04545501935fb2ee5ee32e14e197b6
Andrew DGalga <ex.spake@gmail.com>
2023-09-11 10:51:41 -0500

    redid and built out JS. I forgot I wanted to use jQuery, and overall I did not go into this project with a solid plan for how I would be changing the buttons. The original plan didn't perform well, the code I had before didn't work with scaling, this new thing I've created works ok. earmarked for replacement at some uncertain date.

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const btnTopLeft = document.getElementById('top_left');
-const btnTopRight = document.getElementById('top_right');
-const btnBottom = document.getElementById('bottom');
+const navTopLeft = document.getElementById('top_left');
+const navTopRight = document.getElementById('top_right');
+const navBottom = document.getElementById('bottom');
 const anchorWelcome = document.getElementById('welcome');
 const anchorProjects = document.getElementById('projects');
 const anchorContact = document.getElementById('contact');
 
-console.log(anchorWelcome.getBoundingClientRect().y);
-console.log(anchorProjects.getBoundingClientRect().y);
-console.log(anchorContact.getBoundingClientRect().y);
+const setNav = (targetNav, dataSource) => {
+    targetNav.innerText = dataSource.dataset.name;
+    targetNav.href = dataSource.dataset.pointer;
+};
 
-window.addEventListener('scroll', () => {
-    if(window.scrollY >= anchorContact.getBoundingClientRect().y){
-        btnBottom.innerText = anchorContact.dataset.name;
-        btnBottom.href = anchorContact.dataset.pointer;
-
-
+$(window).scroll(function() {
+    if(($(window).scrollTop() + $(window).height()) > ($(document).height() - $('#contact').height() * 2)){
+        setNav(navBottom, anchorContact);
+        setNav(navTopLeft, anchorWelcome);
+        setNav(navTopRight, anchorProjects);
+    } else if($(window).scrollTop() < ($('#welcome').offset().top * 2)) {
+        setNav(navBottom, anchorWelcome);
+        setNav(navTopLeft, anchorProjects);
+        setNav(navTopRight, anchorContact);
+    } else {
+        setNav(navBottom, anchorProjects);
+        setNav(navTopLeft, anchorContact);
+        setNav(navTopRight, anchorWelcome);
     }
 });
