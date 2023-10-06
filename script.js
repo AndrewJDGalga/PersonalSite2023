bbdd37e0506e0a00971ab6efa85c4b8959f2378b
Andrew DGalga <ex.spake@gmail.com>
2023-10-06 11:01:13 -0500

    corrected front-end, tinkered on resolving mobile scrolling bug but not able to find solution atm, will have to circle back, small change for now.

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const closeMobileMenu = () => {
 }
 
 $(window).scroll( () => {
-    if(($(window).scrollTop() + $(window).height()) > ($(document).height() - $('#contact').height() * 2)){
+    if(($(window).scrollTop() + $(window).height()) >= ($(document).height() - $('#contact').height() * 2)){
         setCurNavDesktop(navBottom, anchorContact);
         setCurNavDesktop(navTopLeft, anchorWelcome);
         setCurNavDesktop(navTopRight, anchorProjects);
